c4d519f292d3078722a84f5003f5a5aaee3f8630
Daniil Kuzjomin <93324329+DaniilKuzjomin@users.noreply.github.com>
2023-03-16 10:33:11 +0200

    Update RGB_Slider_Page.xaml.cs

--- TARpv21_Elemendid/TARpv21_Elemendid/RGB_Slider_Page.xaml.cs
+++ TARpv21_Elemendid/TARpv21_Elemendid/RGB_Slider_Page.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,70 +12,46 @@ namespace TARpv21_Elemendid
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RGB_Slider_Page : ContentPage
     {
-        Label rlbl, blbl, glbl;
+
         BoxView box;
-        Slider rsl, bsl, gsl;
-        int r, g, b;
+        List<Slider> sliders = new List<Slider>();
+        List<Label> labels = new List<Label>();
         Stepper stp;
         Random rnd;
         Button btn;
-        public RGB_Slider_Page()
+        public RGB_Slider_Page2()
         {
             rnd = new Random();
             box = new BoxView
             {
                 Color = Color.Red,
-                CornerRadius = 150,
-                WidthRequest = 200,
-                HeightRequest = 200,
+                CornerRadius = 0,
+                WidthRequest = 320,
+                HeightRequest = 320,
                 HorizontalOptions = LayoutOptions.CenterAndExpand,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
-            rlbl = new Label
+            for (int i = 0; i < 3; i++)
             {
-                Text = "..",
-                TextColor = Color.Red
-            };
-            rsl = new Slider
-            {
-                Minimum = 0,
-                Maximum = 255,
-                Value = 30,
-                MinimumTrackColor = Color.Red,
-                MaximumTrackColor = Color.Yellow,
-                ThumbColor = Color.Orange
-            };
-            rsl.ValueChanged += OnSliderValueChanged;
-            blbl = new Label
-            {
-                Text = "...",
-                TextColor = Color.Blue
-            };
-            bsl = new Slider
-            {
-                Minimum = 0,
-                Maximum = 255,
-                Value = 30,
-                MinimumTrackColor = Color.Red,
-                MaximumTrackColor = Color.Yellow,
-                ThumbColor = Color.Orange
-            };
-            bsl.ValueChanged += OnSliderValueChanged;
-            glbl = new Label
-            {
-                Text = "...",
-                TextColor = Color.Green
-            };
-            gsl = new Slider
-            {
-                Minimum = 0,
-                Maximum = 255,
-                Value = 30,
-                MinimumTrackColor = Color.Red,
-                MaximumTrackColor = Color.Yellow,
-                ThumbColor = Color.Orange
-            };
-            gsl.ValueChanged += OnSliderValueChanged;
+                var label = new Label
+                {
+                    TextColor = i == 0 ? Color.Red : i == 1 ? Color.Green : Color.Blue
+                };
+                labels.Add(label);
+
+                var slider = new Slider
+                {
+                    Minimum = 0,
+                    Maximum = 255,
+                    Value = 30,
+                    MinimumTrackColor = Color.Red,
+                    MaximumTrackColor = Color.Yellow,
+                    ThumbColor = Color.Orange
+                };
+                slider.ValueChanged += OnSliderValueChanged;
+                sliders.Add(slider);
+            }
+
             stp = new Stepper
             {
                 Minimum = 0,
@@ -90,26 +66,40 @@ namespace TARpv21_Elemendid
                 Text = "Random color",
             };
             btn.Clicked += Random_Clicked;
-            
-            StackLayout st = new StackLayout
+
+            StackLayout st = new StackLayout();
+            st.Children.Add(box);
+            foreach (var slider in sliders)
             {
-                Children = { box, rsl, rlbl, gsl, glbl, bsl, blbl, stp, btn }
-            };
+                st.Children.Add(slider);
+                st.Children.Add(labels[sliders.IndexOf(slider)]);
+            }
+            st.Children.Add(stp);
+            st.Children.Add(btn);
+
             Content = st;
         }
 
         private void Random_Clicked(object sender, EventArgs e)
         {
-            r = rnd.Next(0, 255);
-            g = rnd.Next(0, 255);
-            b = rnd.Next(0, 255);
-            rlbl.Text = String.Format("Red = {0:X2}", r);
-            blbl.Text = String.Format("Blue = {0:X2}", b);
-            glbl.Text = String.Format("Green = {0:X2}", g);
-            box.Color = Color.FromRgb(r, b, g);
-            rsl.Value = r;
-            gsl.Value = g;
-            bsl.Value = b;
+            int r = rnd.Next(0, 255);
+            int g = rnd.Next(0, 255);
+            int b = rnd.Next(0, 255);
+            if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255)
+            {
+                labels[0].Text = String.Format("Red = {0:X2}", r);
+                labels[1].Text = String.Format("Green = {0:X2}", g);
+                labels[2].Text = String.Format("Blue = {0:X2}", b);
+            }
+            else
+            {
+                // !
+            }
+            box.Color = Color.FromRgb(r, g, b);
+            foreach (var slider in sliders)
+            {
+                slider.Value = sliders.IndexOf(slider) == 0 ? r : sliders.IndexOf(slider) == 1 ? g : b;
+            }
         }
 
         private void Stp_ValueChanged(object sender, ValueChangedEventArgs e)
@@ -121,20 +111,20 @@ namespace TARpv21_Elemendid
 
         void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
-
-            if (sender == rsl)
+            int r = (int)sliders[0].Value;
+            int g = (int)sliders[1].Value;
+            int b = (int)sliders[2].Value;
+            if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255)
             {
-                rlbl.Text = String.Format("Red = {0:X2}", (int)args.NewValue);
+                labels[0].Text = String.Format("Red = {0:X2}", r);
+                labels[1].Text = String.Format("Green = {0:X2}", g);
+                labels[2].Text = String.Format("Blue = {0:X2}", b);
             }
-            else if (sender == bsl)
+            else
             {
-                blbl.Text = String.Format("Blue = {0:X2}", (int)args.NewValue);
+                // !
             }
-            else if (sender == gsl)
-            {
-                glbl.Text = String.Format("Green = {0:X2}", (int)args.NewValue);
-            }
-            box.Color = Color.FromRgb((int)rsl.Value, (int)gsl.Value, (int)bsl.Value);
+            box.Color = Color.FromRgb(r, g, b);
         }
     }
 }
